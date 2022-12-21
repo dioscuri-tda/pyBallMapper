b2a6f5d84c13561acac78644f64560e46aa34ed2
Davide Gurnari <45945769+dgurnari@users.noreply.github.com>
2022-12-21 17:36:54 +0100

    added knn ball mapper

--- pyballmapper/ballmapper.py
+++ pyballmapper/ballmapper.py
@@ -9,92 +9,232 @@ import warnings
 
 from tqdm.notebook import tqdm
 
-class BallMapper():
-    def __init__(self, X, epsilon, orbits=None, distance=None, order=None, distance_matrix = False, dbg=False):
-        
-        self.epsilon = epsilon
-        if not distance_matrix:
-            n_points = len(X)
-        else:
+
+
+def _find_landmarks_balls(X, eps, orbits=None, metric=None, order=None, dbg=False):
+
+    if metric == 'precomputed':
             n_points = X.shape[0]
+    else:
+        n_points = len(X)
 
-        # set the distance function
-        
-        f = lambda i : X[i]
-        if not distance and not distance_matrix:
-            distance = lambda x, y : np.linalg.norm(x - y)
-            if dbg: print('using euclidean distance')
-        elif not distance and distance_matrix:
-            distance = lambda x, y : X[x,y]
-            f = lambda i : i
-        else:
-            if dbg: print('using custom distance {}'.format(distance))
+    # set the distance function
+    f = lambda i : X[i]
+    if metric == 'euclidean':
+        distance = lambda x, y : np.linalg.norm(x - y)
 
-        # set the orbits
-        if orbits is None:
+    elif metric == 'precomputed':
+        distance = lambda x, y : X[x,y]
+        f = lambda i : i
+        if dbg: print('using precomputed distance matrix')
+
+    else:
+        distance = metric
+        if dbg: print('using custom distance {}'.format(distance))
+
+    # set the orbits
+    if orbits is None:
+        points_have_orbits = False
+    elif (type(orbits) is np.ndarray) or (type(orbits) is list):
+        if len(orbits) != n_points:
             points_have_orbits = False
-        elif type(orbits) is np.ndarray  or (type(orbits) is list):
-            if len(orbits) != n_points:
-                points_have_orbits = False
-                warnings.warn("Warning........... orbits is not compatible with points, ignoring it")
-            else:
-                points_have_orbits = True
+            warnings.warn("Warning........... orbits is not compatible with points, ignoring it")
         else:
-            warnings.warn("Warning........... orbits should be a list or a numpy array, ignoring it")
-            points_have_orbits = False
-        
-        # find landmark points
-        landmarks = {} # dict of points {idx_v: idx_p, ... }
-        centers_counter = 1 # TODO change it to 0
-        
-        # check wheter order is a list of lenght = len(points)
-        # otherwise use the defaut ordering
-        
-        if order:
-            if len(np.unique(order)) != n_points:
-                warnings.warn("Warning........... order is not compatible with points, using default ordering")
-                order = range(n_points)
-        else:
+            points_have_orbits = True
+    else:
+        warnings.warn("Warning........... orbits should be a list or a numpy array, ignoring it")
+        points_have_orbits = False
+    
+    # find landmark points
+    landmarks = {} # dict of points {idx_v: idx_p, ... }
+    centers_counter = 0
+    
+    # check wheter order is a list of lenght = len(points)
+    # otherwise use the defaut ordering
+    if order:
+        if len(np.unique(order)) != n_points:
+            warnings.warn("Warning........... order is not compatible with points, using default ordering")
             order = range(n_points)
+    else:
+        order = range(n_points)
+    
+    if dbg:
+        print('Finding vertices...')
+    
+    pbar = tqdm(order, disable=not(dbg))
+    
+    for idx_p in pbar:
         
-        if dbg:
-            print('Finding vertices...')
+        # current point
+        p = f(idx_p)
         
-        pbar = tqdm(order, disable=not(dbg))
+        pbar.set_description("{} vertices found".format(centers_counter))
         
-        for idx_p in pbar:
-            
-            # current point
-            p = f(idx_p)
-            
-            pbar.set_description("{} vertices found".format(centers_counter))
-            
-            is_covered = False
+        is_covered = False
 
-            for idx_v in landmarks:
-                if distance(p, f(landmarks[idx_v])) <= epsilon:
-                    is_covered = True
-                    break
+        for idx_v in landmarks:
+            if distance(p, f(landmarks[idx_v])) <= eps:
+                is_covered = True
+                break
 
-            if not is_covered:
-                landmarks[centers_counter] = idx_p
-                centers_counter += 1
-                # add points in the orbit
-                if points_have_orbits:
-                    for idx_p_o in orbits[idx_p]:
-                        if idx_p_o != idx_p:
-                            landmarks[centers_counter] = idx_p_o
-                            centers_counter += 1
+        if not is_covered:
+            landmarks[centers_counter] = idx_p
+            centers_counter += 1
+            # add points in the orbit
+            if points_have_orbits:
+                for idx_p_o in orbits[idx_p]:
+                    if idx_p_o != idx_p:
+                        landmarks[centers_counter] = idx_p_o
+                        centers_counter += 1
+
+    # compute points_covered_by_landmarks
+    if dbg:
+        print('Computing points_covered_by_landmarks...')
+    points_covered_by_landmarks = dict()
+    for idx_v in tqdm(landmarks, disable=not(dbg)):
+        points_covered_by_landmarks[idx_v] = []
+        for idx_p in order:
+            if distance(f(idx_p), f(landmarks[idx_v])) <= eps:
+                points_covered_by_landmarks[idx_v].append(idx_p)
+
+    return landmarks, points_covered_by_landmarks
+
+
+
+
+
+
+
+
+
+
+
+def _find_landmarks_knn(X, knn, metric='euclidean', orbits=None, order=None, dbg=False):
+
+    from sklearn.neighbors import NearestNeighbors
+    neigh = NearestNeighbors(n_neighbors=knn, algorithm='auto', metric=metric).fit(X)
+    
+    # check wheter order is a list of lenght = len(points)
+    # otherwise use the defaut ordering
+
+    if metric == 'precomputed':
+        n_points = X.shape[0]
+    else:
+        n_points = len(X)
+
+    if order:
+        if len(np.unique(order)) != n_points:
+            warnings.warn("Warning........... order is not compatible with points, using default ordering")
+            order = range(n_points)
+    else:
+        order = range(n_points)
+
+
+    # set the orbits
+    if orbits is None:
+        points_have_orbits = False
+    elif (type(orbits) is np.ndarray) or (type(orbits) is list):
+        if len(orbits) != n_points:
+            points_have_orbits = False
+            warnings.warn("Warning........... orbits is not compatible with points {} != {}, ignoring it".format(len(orbits), n_points))
+        else:
+            points_have_orbits = True
+    else:
+        warnings.warn("Warning........... orbits should be a list or a numpy array, ignoring it")
+        points_have_orbits = False
+
+
+    
+    if dbg:
+        print('Finding vertices...')
+    
+    points_covered = dict()
+    for i in range(len(X)):
+        points_covered[i] = False
+
+    if metric=='precomputed':
+        f = lambda i : i
+    else:
+        f = lambda i : X[i]
+
+     # find landmark points
+    landmarks = {} # dict of points {idx_v: idx_p, ... }
+    points_covered_by_landmarks = dict()
+    centers_counter = 0
+
+    pbar = tqdm(order, disable=not(dbg))
+    for idx_p in pbar:
+        pbar.set_description("{} vertices found".format(centers_counter))
+        
+        if points_covered[idx_p]:
+            continue
+        else:
+            # current point is not covered
+            points_covered_by_landmarks[centers_counter] = neigh.kneighbors([f(idx_p)], return_distance=False)[0]
+            for n in points_covered_by_landmarks[centers_counter]:
+                points_covered[n] = True
+
+            landmarks[centers_counter] = idx_p
+            centers_counter += 1
+
+            # add points in the orbit
+            if points_have_orbits:
+                for idx_p_o in orbits[idx_p]:
+                    if idx_p_o != idx_p:
+                        points_covered_by_landmarks[centers_counter] = neigh.kneighbors([f(idx_p_o)], return_distance=False)[0]
+                        for n in points_covered_by_landmarks[centers_counter]:
+                            points_covered[n] = True
+
+                        landmarks[centers_counter] = idx_p_o
+                        centers_counter += 1
+
+    return landmarks, points_covered_by_landmarks
+
+
+
+
+
+
+def find_landmarks(X, eps=None, orbits=None, metric=None, order=None, knn=False, dbg=False):
+
+    if knn:
+        landmarks, points_covered_by_landmarks = _find_landmarks_knn(X=X, knn=knn, metric=metric, 
+                                                                     orbits=orbits, order=order, 
+                                                                     dbg=dbg)
+
+    else:
+        landmarks, points_covered_by_landmarks = _find_landmarks_balls(X, eps, orbits, metric, order, dbg)
+
+    return landmarks, points_covered_by_landmarks
+
+
+
+class BallMapper():
+    def __init__(self, X, eps=None, orbits=None, metric='euclidean', order=None, knn=None, dbg=False):
+
+        """Create a BallMapper graph from vector array or distance matrix.
+
+        Parameters
+        ----------
+        eps : float, default=None
+            The radius of the balls.
+        
+        metric : str, or callable, default='euclidean'
+            The metric to use when calculating distance between instances in a
+            feature array. If metric is a string or callable, it must be one of
+            the options allowed by :func:`sklearn.metrics.pairwise_distances` for
+            its metric parameter.
+            If metric is "precomputed", X is assumed to be a distance matrix and
+            must be square. X may be a :term:`sparse graph`, in which
+            case only "nonzero" elements may be considered neighbors for DBSCAN.
+
+            """
+        
+        self.eps = eps
+
+        landmarks, self.points_covered_by_landmarks = find_landmarks(X, eps, orbits, metric, 
+                                                                     order, knn, dbg)
                     
-        # compute points_covered_by_landmarks
-        if dbg:
-            print('Computing points_covered_by_landmarks...')
-        self.points_covered_by_landmarks = dict()
-        for idx_v in tqdm(landmarks, disable=not(dbg)):
-            self.points_covered_by_landmarks[idx_v] = []
-            for idx_p in order:
-                if distance(f(idx_p), f(landmarks[idx_v])) <= epsilon:
-                    self.points_covered_by_landmarks[idx_v].append(idx_p)
                 
         # find edges
         if dbg:
@@ -106,7 +246,6 @@ class BallMapper():
                     edges.append([idx_v, idx_u])
                 
 
-                    
         # create Ball Mapper graph
         if dbg:
             print('Creating Ball Mapper graph...')
@@ -128,7 +267,7 @@ class BallMapper():
             # TODO move the display sizing to the plotting utils
             # rescale the size for display
             # self.Graph.nodes[node]['size rescaled'] = MAX_SCALE*self.Graph.nodes[node]['size']/MAX_NODE_SIZE + MIN_SCALE 
-            self.Graph.nodes[node]['color'] = 'r' # TODO change the defaul color
+            # self.Graph.nodes[node]['color'] = 'r' # TODO change the defaul color
                 
         if dbg:
             print('Done')
@@ -137,11 +276,11 @@ class BallMapper():
     def add_coloring(self, coloring_df, add_std=False):
         # for each column in the dataframe compute the mean across all nodes and add it as mean attributes
         for node in self.Graph.nodes:
-            for name, avg in coloring_df.loc[self.Graph.nodes[node]['points covered']].mean().iteritems():
+            for name, avg in coloring_df.loc[self.Graph.nodes[node]['points covered']].mean().items():
                 self.Graph.nodes[node][name] = avg
             # option to add the standar deviation on each node
             if add_std:
-                for name, std in coloring_df.loc[self.Graph.nodes[node]['points covered']].std().iteritems():
+                for name, std in coloring_df.loc[self.Graph.nodes[node]['points covered']].std().items():
                     self.Graph.nodes[node]['{}_std'.format(name)] = std
 
             
